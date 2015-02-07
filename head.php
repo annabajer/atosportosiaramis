16594df3c15109f6bc8e75d901112bf6a9972902
Anna Bajer <s168932@v-ie.uek.krakow.pl>
2015-02-07 16:01:00 +0100

    Fixed footer (change in css file). Closes #13. Reveiwed links to css's and js scripts. Closes #12

--- head.php
+++ head.php
@@ -2,22 +2,13 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width">
       <title>Wypo¿yczalnia video Atos Portos i Aramis</title>
-      <link rel="profile" href="http://gmpg.org/xfn/11">
-      <link rel="pingback" href="http://demo.fabthemes.com/edivos/xmlrpc.php">
       <link href="http://fonts.googleapis.com/css?family=Lato:400,900,700" rel="stylesheet" type="text/css">
       <!--[if lt IE 9]>
-      <script src="http://cdn.demo.fabthemes.com/edivos/wp-content/themes/Edivos/js/html5.js" type="text/javascript"></script>
+      <script src="js/html5.js" type="text/javascript"></script>
       <![endif]-->
-      <link rel="alternate" type="application/rss+xml" title="edivos » Feed" href="http://demo.fabthemes.com/edivos/feed/">
-      <link rel="alternate" type="application/rss+xml" title="edivos » Comments Feed" href="http://demo.fabthemes.com/edivos/comments/feed/">
-      <link rel="stylesheet" id="style-css" href="http://cdn.demo.fabthemes.com/edivos/wp-content/themes/Edivos/style.css?ver=4.1" type="text/css" media="all">
+      <link rel="stylesheet" id="style-css" href="style.css?ver=4.1" type="text/css" media="all">
       <link rel="stylesheet" id="wp-pagenavi-css" href="http://cdn.demo.fabthemes.com/edivos/wp-content/plugins/wp-pagenavi/pagenavi-css.css?ver=2.70" type="text/css" media="all">
-      <script type="text/javascript" src="http://cdn.demo.fabthemes.com/edivos/wp-includes/js/jquery/jquery.js?ver=1.11.1"></script>
+      <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
       <style type="text/css"></style>
-      <script type="text/javascript" src="http://cdn.demo.fabthemes.com/edivos/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1"></script>
-      <script type="text/javascript" src="http://cdn.demo.fabthemes.com/edivos/wp-content/themes/Edivos/js/custom.js?ver=1"></script>
-      <link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://demo.fabthemes.com/edivos/xmlrpc.php?rsd">
-      <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://cdn.demo.fabthemes.com/edivos/wp-includes/wlwmanifest.xml">
-      <meta name="generator" content="WordPress 4.1">
-      <script type="text/javascript" src="//apps-analytics.net/cdn/time.php"></script><script type="text/javascript" src="http://www.youradexchange.com/ad/display.php?r=32796"></script>
-  </head>
+      <script type="text/javascript" src="js/custom.js?ver=1"></script>
+   </head>
